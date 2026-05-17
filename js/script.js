1f70a7423c94792fad44fac350a8542168df5ce3
mm2645700-byte <mm2645700@gmail.com>
2026-05-17 20:34:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+new TradingView.widget({
+    "autosize": true,
+    "symbol": "BINANCE:ETHUSDT",
+    "interval": "15",
+    "timezone": "Etc/UTC",
+    "theme": "dark",
+    "style": "1",
+    "locale": "en",
+    "enable_publishing": false,
+    "hide_side_toolbar": false,
+    "allow_symbol_change": true,
+    "container_id": "tradingview_eth"
+});
